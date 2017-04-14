ed1f81dc7c90290e155f12d968b6effea7383e1e
Rocco Barbini <roccobarbi@gmail.com>
2017-04-14 13:46:49 +0200

    Predatore added

--- src/predatorieprede02/Predatore.java
+++ src/predatorieprede02/Predatore.java
@@ -1,5 +1,7 @@
 package predatorieprede02;
 
+import predatorieprede01.Preda;
+
 public class Predatore extends Organismo {
 	
 	// Private fields
@@ -91,18 +93,40 @@ public class Predatore extends Organismo {
 	// Public methods
 	
 	/**
-	 * It kills a prey and eats it
-	 */
-	public void eat(){
-		// TODO
-	}
-	
-	/**
 	 * It overloads the chooseMove() method for Organismo  account for the need to eat.
+	 * @param	grid	an array of 8 Organisms or null representing the surrounding area (an organism can be used as filler for unmovable positions)
+	 * @return	the direction where it should move (1 up, 3 right, 5 down, 7 left) or -1 if none is found, or -10 if the beast is dead	
 	 */
 	public int chooseMove(Organismo [] grid){
-		// TODO
-		return -1;
+		int available = 0;
+		int destination = 0;
+		int move = -10; // Default: the beast is dead and should be removed or managed
+		if(isAlive){
+			move = -1; // New default: no move.
+			// Odd cells in the grid are those where the predator can move
+			for(int i = 1; i < 8; i += 2){ 
+				if(grid[i] == null) available++;
+				if(grid[i] instanceof Preda){ // Prey found, let's go for the kill!
+					resetDaysUntilStarve();
+					move = i;
+					break; // No need to check any other direction for this lazy, naive predator.
+				}
+			}
+			// If there are available cells, chose one and move there
+			if(available > 0 && move == -1){ // No prey has been found AND there are empty cells
+				destination = (int)(Math.random() * available); // 0, 1, 2 or 3
+				for(int i = 1; i < 8 && destination >= 0; i += 2){ 
+					if(destination == 0) move = i;
+					if(grid[i] == null) destination--;
+				}
+				reduceDaysUntilStarve(); // If no prey was found, the beast is one day closer to starvation
+			}
+			if(getDaysUntilStarve() == 0){ // The beast is dead, long live the beast!
+				setIsAlive(false);
+				move = -10;
+			}
+		}
+		return move;
 	}
 
 }

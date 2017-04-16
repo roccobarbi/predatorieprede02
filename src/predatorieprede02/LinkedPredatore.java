367f91186640396a112377f3fa150acc0249d5e2
Rocco Barbini <roccobarbi@gmail.com>
2017-04-16 18:18:22 +0200

    LinkedPredatore class outlined

---
+++ src/predatorieprede02/LinkedPredatore.java
@@ -0,0 +1,22 @@
+package predatorieprede02;
+
+public class LinkedPredatore extends LinkedOrganism {
+
+	// Private fields
+	private Predatore self; // Overloaded
+	
+	public LinkedPredatore() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	// Public methods
+	public void act(){ // Overloaded
+		// TODO
+	}
+	
+	public Predatore reveal(){ // Overloaded
+		//TODO outputs a safe copy of the occupant
+		return null;
+	}
+
+}

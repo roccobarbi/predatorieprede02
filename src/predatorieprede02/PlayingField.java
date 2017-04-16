95ff4ce914c0b31fc742283a2a86213df7c5441b
Rocco Barbini <roccobarbi@gmail.com>
2017-04-16 18:07:06 +0200

    PlayingField class outlined

---
+++ src/predatorieprede02/PlayingField.java
@@ -0,0 +1,27 @@
+package predatorieprede02;
+
+public class PlayingField {
+
+	// private fields
+	private LinkedOrganism occupant[][]; // A grid of LinkedOrganisms
+
+	public PlayingField() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	// Public methods
+	public Organismo[] lookAround(int posX, int posY){
+		Organismo output[] = {null, null, null, null, null, null, null, null};
+		// TODO provides an array of 8 Organisms around a cell
+		return output;
+	}
+	
+	public void move(int fromX, int fromY, int toX, int toY){
+		// TODO moves the content of a cell to another
+	}
+	
+	public void spawn(int posX, int posY, Organismo pup){
+		// TODO sets an Organismo to a cell
+	}
+
+}

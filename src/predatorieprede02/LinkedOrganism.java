b6d5111ae96fe2a25954c5c4a4c6347d79a2e601
Rocco Barbini <roccobarbi@gmail.com>
2017-04-16 18:14:50 +0200

    LinkedOrganisms class outlined and LinkedOrganism change to account for it

--- src/predatorieprede02/LinkedOrganism.java
+++ src/predatorieprede02/LinkedOrganism.java
@@ -9,6 +9,25 @@ public class LinkedOrganism {
 	private int posX;
 	private int posY;
 	private PlayingField field;
+	
+	// Accessors
+	public void setNext(LinkedOrganism newNext){
+		next = newNext;
+	}
+	
+	public void setPrev(LinkedOrganism newPrev){
+		prev = newPrev;
+	}
+	
+	public LinkedOrganism getNext(){
+		return next; // This must not be a safe copy, as it can be used to operate on the list
+	}
+	
+	public LinkedOrganism getPrev(){
+		return prev; // This must not be a safe copy, as it can be used to operate on the list
+	}
+	
+	// Constructors
 
 	public LinkedOrganism() {
 		// TODO Auto-generated constructor stub

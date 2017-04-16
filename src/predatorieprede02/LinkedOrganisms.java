af2cc8bdc08dcba0abd1184caf22722ed931f38c
Rocco Barbini <roccobarbi@gmail.com>
2017-04-16 18:18:03 +0200

    LinkedOrganisms class really added

---
+++ src/predatorieprede02/LinkedOrganisms.java
@@ -0,0 +1,31 @@
+package predatorieprede02;
+
+public class LinkedOrganisms {
+
+	// private members
+	private Organismo first;
+	private Organismo last;
+	private int length;
+
+	// Accessors
+	
+	public int getLength(){
+		return length;
+	}
+	
+	// Constructors
+	
+	public LinkedOrganisms() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	// public methods
+	public void add(Organismo organismo){
+		// TODO creates a new LinkedOrganism and adds it at the end of the list
+	}
+	
+	public void remove(LinkedOrganism organismo){
+		// TODO removes internal link to organismo
+	}
+
+}

281b09760dde629aa71dfc97711e15511f4150fe
Rocco Barbini <roccobarbi@gmail.com>
2017-04-17 07:44:06 +0200

    improved constructors and accessors

--- src/predatorieprede02/LinkedOrganism.java
+++ src/predatorieprede02/LinkedOrganism.java
@@ -9,6 +9,7 @@ public class LinkedOrganism {
 	private int posX;
 	private int posY;
 	private PlayingField field;
+	private LinkedOrganisms list;
 	
 	// Accessors
 	public void setNext(LinkedOrganism newNext){
@@ -27,25 +28,53 @@ public class LinkedOrganism {
 		return prev; // This must not be a safe copy, as it can be used to operate on the list
 	}
 	
+	public void setPosX(int posX){
+		this.posX = posX;
+	}
+	
+	public int getPosX(){
+		return this.posX;
+	}
+	
+	public void setPosY(int posY){
+		this.posY = posY;
+	}
+	
+	public int getPosY(){
+		return this.posY;
+	}
+	
+	public void setList(LinkedOrganisms list){
+		this.list = list;
+	}
+	
+	public LinkedOrganisms getList(){
+		return this.list;
+	}
+	
 	// Constructors
 
 	public LinkedOrganism() {
 		this(null, -1, -1, null); // Negative positions imply that the organism hasn't been placed on the field
 	}
 	
-	public LinkedOrganism(Organismo organismo, int posX, int posY, PlayingField field) {
+	public LinkedOrganism(Organismo organismo, int posX, int posY, PlayingField field){
+		this(organismo, posX, posY, field, null);
+	}
+	
+	public LinkedOrganism(Organismo organismo, int posX, int posY, PlayingField field, LinkedOrganisms list) {
 		this.self = organismo;
 		this.posX = posX;
 		this.posY = posY;
 		this.field = field;
+		this.list = list;
 	}
 	
 	// Public methods
 	
 	public void kill(){
 		field.remove(posX, posY); // Remove the current element from the playing field
-		
-		// TODO eliminates the links to self from the field and the linked list
+		list.remove(this); // Cleanly removes the current element from the list
 	}
 	
 	public void act(){

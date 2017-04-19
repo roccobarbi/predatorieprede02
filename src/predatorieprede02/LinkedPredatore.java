6a273c55fbe4e8e38cf9c521f298c655c09683a2
Rocco Barbini <roccobarbi@gmail.com>
2017-04-19 20:14:23 +0200

    accessors and constructors added

--- src/predatorieprede02/LinkedPredatore.java
+++ src/predatorieprede02/LinkedPredatore.java
@@ -3,12 +3,41 @@ package predatorieprede02;
 public class LinkedPredatore extends LinkedOrganism {
 
 	// Private fields
+
 	private Predatore self; // Overloaded
 	
-	public LinkedPredatore() {
-		// TODO Auto-generated constructor stub
+	// Accessors
+	
+	/**
+	 * @return the self
+	 */
+	public Predatore getSelf() {
+		return self;
+	}
+
+	/**
+	 * @param self the self to set
+	 */
+	private void setSelf(Predatore self) {
+		if(self instanceof Predatore) // To avoid that a null value overwrites a legitimate one
+			this.self = self;
 	}
 	
+	// Constructors
+
+	public LinkedPredatore() {
+		this(null, -1, -1, null); // Negative positions imply that the organism hasn't been placed on the field
+	}
+	
+	public LinkedPredatore(Predatore predatore, int posX, int posY, PlayingField field){
+		this(predatore, posX, posY, field, null);
+	}
+	
+	public LinkedPredatore(Predatore predatore, int posX, int posY, PlayingField field, LinkedOrganisms list) {
+		super(predatore, posX, posY, field, list);
+		setSelf(predatore);
+	}
+
 	// Public methods
 	public void act(){ // Overloaded
 		// TODO

a9aa6a0a86556f505a3a31dfdffeaf893ad0be3b
Rocco Barbini <roccobarbi@gmail.com>
2017-04-22 15:27:51 +0200

    first (currently wrong) version of play added

---
+++ src/predatorieprede02/play.java
@@ -0,0 +1,42 @@
+package predatorieprede02;
+
+import java.util.Scanner;
+
+public class play {
+
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in); 
+		
+		LinkedOrganisms prede = new LinkedOrganisms();
+		LinkedOrganisms predatori = new LinkedOrganisms();
+		PlayingField field = new PlayingField();
+		LinkedOrganism preda;
+		LinkedPredatore predatore;
+		Preda predaPup;
+		Predatore predatorePup;
+		
+		// Default mode.
+		// TODO add different game modes
+		
+		for(int i = 0; i < 5; i++){
+			predatorePup = new Predatore();
+			predatore = new LinkedPredatore(predatorePup, 0, i, field, predatori);
+		}
+		
+		for(int i = 0; i < 100; i++){
+			predaPup = new Preda();
+			preda = new LinkedOrganism(predaPup, (1 + i / 20), (i % 20), field, prede);
+		}
+		
+		field.shuffle();
+		field.print();
+		
+		for(int i = 0; i < 10; i++){
+			keyboard.nextLine();
+			predatori.act();
+			prede.act();
+			field.print();
+		}
+	}
+
+}
